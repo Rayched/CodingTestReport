ce7880cf97cde680692313b4ce52a93fe1eeb2fa
Rayched <ny2401@naver.com>
2023-09-13 11:50:45 +0900

    코딩 테스트 레포트 웹 페이지 기본 뼈대 제작, git 연결

---
+++ src/MultipleNumber.ts
@@ -0,0 +1,23 @@
+//프로그래머스 코딩테스트
+//LV0 n의 배수
+
+/** 문제 설명 
+ * 정수 num, n이 매개변수로 주어질 때
+ * num이 n의 배수이면 숫자 1을 return하고
+ * num이 n의 배수가 아니라면 숫자 0을 return하는
+ * solution 함수를 완성해주세요.
+ 
+ ** 제한 사항
+  * 2 <= num <= 100
+  * 2 <= n <= 9
+   
+ ** 입출력 예시
+  num n result 
+  98  2   1
+  34  3   0 
+*/
+
+function solution(num:number, n:number){
+    let answer = 0;
+    return answer;
+}
